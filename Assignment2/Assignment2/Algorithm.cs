99e00038590f2c1fb1543e1bd9808ddbaa1658c2
ratihng <ngestrini@gmail.com>
2017-03-22 19:03:16 +0100

    generateInitialPopulation was moved to Metaheuristic. Other classes will be updated later.

--- Assignment2/Assignment2/Algorithm.cs
+++ Assignment2/Assignment2/Algorithm.cs
@@ -16,5 +16,121 @@ namespace Assignment2
         }
 
         // MLS - ILS - GLS
+        public void MLS()
+        {
+            int iteration = 0;
+            List<Individual> localoptima = new List<Individual>();
+
+            List<Individual> initialsolutions = h.generateInitialPopulation(50);
+
+            for (int p = 0; p < 2500 / initialsolutions.Count; p++)
+            {
+                foreach(Individual i in initialsolutions)
+                {
+                    Individual solution = i;
+                    Individual newsolution = h.VSN(solution);
+                    while (newsolution.connections >= solution.connections)
+                    {
+                        solution = newsolution;
+                        newsolution = h.VSN(solution);
+                    }
+                    localoptima.Add(newsolution);
+                    iteration++;
+                    Console.WriteLine("[" + iteration + "]" + newsolution.connections);
+                }
+            }
+        }
+
+        public void ILS(int perturbationSize, int timeLimit = 0)
+        {
+            int iteration = 1;
+            int notleft = 0;
+            List<Individual> localoptima = new List<Individual>();
+            Individual solution;
+
+            Individual initialsolution = h.generateInitialPopulation(1)[0];
+            solution = h.VSN(initialsolution);
+            localoptima.Add(solution);
+            Console.WriteLine("[" + iteration + "]" + solution.connections);
+
+            while(localoptima.Count <= 2500)
+            {
+                Individual mutatedsolution = h.PerturbateSolution(solution, perturbationSize);
+                Individual newsolution = h.VSN(mutatedsolution);
+
+                if (newsolution.connections < localoptima[localoptima.Count - 1].connections) //When the new local optimum is better than the current one, ILS continues its search from this new local optimum
+                {
+                    solution = newsolution;
+                    localoptima.Add(solution);
+
+                    iteration++;
+                    Console.WriteLine("[" + iteration + "]" + newsolution.connections);
+                }
+                else //else it simply returns to the previous local optimum and tries again with another mutation
+                {
+                    if (newsolution.connections == localoptima[localoptima.Count - 1].connections) //the search has not left the region of attraction of the perturbed local optimum
+                    {
+                        notleft++;
+                    }
+                    solution = localoptima[localoptima.Count-1];
+                }
+            }
+        }
+
+
+        public void GLS(int popSize, int timeLimit = 0)
+        {
+            List<Individual> initialpopulation = h.generateInitialPopulation(popSize);
+            //incremental: only one population, everytime an offspring created, it competes with worst solution in population
+            //generational: new population (of offspring) replaces the old population
+
+            //keep local optima found = local search
+            List<Individual> newpopulation = new List<Individual>();
+            for(int i = 0; i < popSize; i++)
+            {
+                Individual localoptima = h.VSN(initialpopulation[i]);
+                newpopulation.Add(localoptima);
+            }
+
+            //incremental
+            Random rand = new Random();
+            for(int i = 0; i < 2500; i++)
+            {
+                //parent pair selected at random
+                int random1 = rand.Next(0, popSize);
+                int random2 = random1;
+                while (random2 == random1)
+                    random2 = rand.Next(0, popSize);
+
+                //recombinaton
+                Individual offspring = h.Recombination(newpopulation[random1], newpopulation[random2]);
+
+                //local search
+                Individual temp = h.VSN(offspring);
+
+                //No duplicate solutions allowed in the population??
+                bool has = newpopulation.Any(idv => idv.solution == temp.solution);
+                while(has)
+                    temp = h.VSN(offspring);
+
+                newpopulation.Add(temp);
+
+                //selection (incremental) = competes with worst solution in population
+                newpopulation = newpopulation.OrderBy(idv => idv.connections).Take(popSize).ToList();
+
+                //randomize the population
+                int n = newpopulation.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rand.Next(n + 1);
+                    Individual value = newpopulation[k];
+                    newpopulation[k] = newpopulation[n];
+                    newpopulation[n] = value;
+                }
+            }
+
+        }
+        
     }
 }
--- Assignment2/Assignment2/Metaheuristic.cs
+++ Assignment2/Assignment2/Metaheuristic.cs
@@ -14,11 +14,100 @@ namespace Assignment2
         {
             graph = g;
         }
-        /*
-        public String VSN(String input)
-        { }
 
-        public String Recombination(String input)
-        { }*/
+        public List<Individual> generateInitialPopulation(int n)
+        {
+            List<Individual> population = new List<Individual>();
+
+            int l = graph.graph.Count;
+            Char[] baseIndividual = new Char[l];
+            for (int i = 0; i < l / 2; i++)
+            {
+                baseIndividual[i] = '1';
+                baseIndividual[i + l / 2] = '0';
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                String individual = scrambleToRandomIndividual(baseIndividual);
+                population.Add(new Individual(individual, graph.calculateFitness(individual)));
+            }
+
+            return population;
+        }
+
+        static String scrambleToRandomIndividual(Char[] individual)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            int n = individual.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                Char value = individual[k];
+                individual[k] = individual[n];
+                individual[n] = value;
+            }
+
+            return new String(individual);
+        }
+
+        
+        public Individual VSN(Individual input)
+        { 
+            Random rand = new Random();
+            int random1 = rand.Next(0, input.solution.Length-1);
+            int random2 = random1;
+            while (random2 == random1 || input.solution[random1] == input.solution[random2])
+                random2 = rand.Next(0, input.solution.Length-1);
+
+            char t1 = input.solution[random1];
+            char t2 = input.solution[random2];
+            char[] binary = input.solution.ToCharArray();
+            char temp = binary[random1];
+            binary[random1] = binary[random2];
+            binary[random2] = temp;
+
+            String newsolution = new String(binary);
+
+            return new Individual(newsolution, graph.calculateFitness(newsolution));
+        }
+
+        public Individual PerturbateSolution(Individual input, int size)
+        {
+            Random rand = new Random();
+            int random1 = 0;
+            int random2 = 0;
+            char[] binary = input.solution.ToCharArray();
+
+            for (int i = 0; i < size; i++)
+            {
+                random1 = rand.Next(0, input.solution.Length - 1);
+                random2 = random1;
+                while (random2 == random1 || input.solution[random1] == input.solution[random2])
+                    random2 = rand.Next(0, input.solution.Length - 1);
+
+                char t1 = input.solution[random1];
+                char t2 = input.solution[random2];
+
+                char temp = binary[random1];
+                binary[random1] = binary[random2];
+                binary[random2] = temp;
+            }
+
+            String mutatedsolution = new String(binary);
+            return new Individual(mutatedsolution, graph.calculateFitness(mutatedsolution));
+        }
+
+        public Individual Recombination(Individual parent1, Individual parent2)
+        {
+            //To be added
+
+            return parent1;
+        
+        }
+
+
     }
 }
